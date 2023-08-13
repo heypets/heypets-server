768d7766c3580cd68232c3f714b14806a1e527d2
changminkim-329 <ckd29672@naver.com>
2023-08-13 14:46:21 +0900

    initialize spring project

---
+++ src/main/java/com/heypets/heypetsserver/HeypetsserverApplication.java
@@ -0,0 +1,13 @@
+package com.heypets.heypetsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeypetsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeypetsserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heypets/heypetsserver/HeypetsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heypets.heypetsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeypetsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
