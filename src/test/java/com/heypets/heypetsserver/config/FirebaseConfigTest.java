ccfeccfb9d36a279af0e3383045b1628ff16ce7f
Changmin Kim <ckd29672@naver.com>
2023-08-21 21:59:46 +0900

    Firestorage 연동

--- src/test/java/com/heypets/heypetsserver/config/FirebaseConfigTest.java
+++ src/test/java/com/heypets/heypetsserver/config/FirebaseConfigTest.java
@@ -1,15 +1,26 @@
 package com.heypets.heypetsserver.config;
 
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.cloud.StorageClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class FirebaseConfigTest {
+    @Value("${firebase.service.bucket.url}")
+    private String bucket_url;
     @Test
     public void 파이어스토어연동(){
         Firestore firestore = FirestoreClient.getFirestore();
         System.out.println("Firestore 연동 테스트 성공");
+    }
+
+    @Test
+    public void 파이어스토리지연동(){
+        Bucket bucket = StorageClient.getInstance().bucket(bucket_url);
+        System.out.println("Firestorage 연동 테스트 성공");
     }
 }
